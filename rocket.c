db7aa31aac3bf5b389f6bdcbfd1799cc60c60003
Alper <alperkarasuer@gmail.com>
2018-12-26 00:33:45 +0300

    Update rocket.c

--- rocket.c
+++ rocket.c
@@ -1,39 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define BigG 0.000000000067408L
-#define EarthMass 5973600000000000000000000L
+#include <time.h>
+
+
+
+void delay(double howLong);
 
 int main(int argc, char* argv[])
-{	//Saturn V rocket - F1 Engine
-	const double timestep = 0.000000000000000000000001;
-	const double burnRate = 12890;
-	const double specificImpulse = 263 ;
-	const double dryMass = 208606;
-	const double totalMass = 2970000;
-	double velocity, earthRadius, i;
+{	
+	//Saturn V rocket - F1 Engine
+	const double earthMass =  5.972E24;
+	const double BigG = 6.67408E-11;
+	const double timestep = 0.1; // time resolution
+	const double burnRate = 12890; // 2578 per engine times 5 - wikipedia
+	const double specificImpulse = 263 ; // wikipedia
+	const double totalMass = 2950000;
+	double velocity, earthRadius, time;
 
-	printf("Enter Earth's radius at location: ");
+	printf("Enter Earth's radius at location in meters: ");
 	scanf("%lf",&earthRadius);
-	
-	double gravitationalAcceleration = BigG*EarthMass/(earthRadius*earthRadius);
+
+	double gravitationalAcceleration = BigG*earthMass/(earthRadius*earthRadius);
 	double exhaustVelocity = specificImpulse*gravitationalAcceleration;
-	double escapeVelocity = sqrt((2*BigG*EarthMass)/earthRadius);
+	double escapeVelocity = sqrt((2*BigG*earthMass)/earthRadius);
 
 
-	for(i = 0; velocity <= escapeVelocity; i += 0.000000000000000000000001)
+	for(time = 0; velocity <= escapeVelocity; time += timestep )
 	{
 
-		velocity = exhaustVelocity*log(totalMass/(totalMass-burnRate*i));
+		velocity = exhaustVelocity*log(totalMass/(totalMass-burnRate*time));
+		printf("At time %.3lf seconds speed of the rocket is %.4lf meters per second\n",time,velocity);
+		delay(timestep);
 
 	}
 
-
-	
-	printf("%lf",i);
-
-
-
-
 	return 0;
+}
+
+
+void delay(double howLong)
+{
+	howLong *= 1000;
+	clock_t startTime = clock();
+
+	while(clock() < startTime + howLong);
+
 }
